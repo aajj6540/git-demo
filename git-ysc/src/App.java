96221769dba36fc5dd62aa02e79ed68e446141d0
goott6 <goott6@goott6-PC>
2020-01-10 15:04:40 +0900

    a2

--- git-ysc/src/App.java
+++ git-ysc/src/App.java
@@ -4,10 +4,10 @@ public class App {
 	public static void main(String[] args) {
 
 
-		System.out.println("I'm ");
-		System.out.println("I'm ");
-		System.out.println("I'm ");
-		System.out.println("I'm ");
+	
+		System.out.println("I'maa ");
+		System.out.println("I'mss ");
+		System.out.println("I'm2323 ");
 		
 
 	}
